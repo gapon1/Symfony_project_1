a93c9d5329940b3311311e3e1a4e57111b5eaa2d
Vitaliy <gapon007@ukr.net>
2019-03-05 15:10:40 +0200

    Work with TWIG

--- my_project_name/src/AppBundle/Controller/DefaultController.php
+++ my_project_name/src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,22 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $a = 11122;
+        $someArray = [1,2,"sobaka"];
+        $someValue = false;
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig',
+            ['a' => $a,
+                'someArray' => $someArray,
+                'some_value' => $someValue
+            ]);
+    }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     */
+    public function feddbackAction(){
+        die('Works');
     }
 }
